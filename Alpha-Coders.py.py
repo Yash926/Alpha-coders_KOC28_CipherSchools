8ed84faf3b9d9d80052d73d822ecf133c1cd9289
Yash926 <114800027+Yash926@users.noreply.github.com>
2022-11-25 23:33:06 +0530

    Project 16

---
+++ Alpha-Coders.py.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+print("------------------  Let's See How Many Days You Wasted ------------------\n\n\n")
+
+print("\t[!] Note -----> Date must be in format ( DD/MM/YY )\n\n")
+
+dob = input("\tEnter Your date of Birth :-) ")
+
+pd = input("\tEnter present date       :-) ")
+
+
+start_date = datetime.strptime(pd, "%d/%m/%Y")
+end_date = datetime.strptime(dob, "%d/%m/%Y")
+result = start_date - end_date
+
+print("\n\n")
+print("-"*70)
+print(f"\t Today {result.days} days completed of Your Life")
+print("-"*70)
